d8c247e55edeffb0393d0322f5978df28135c1ed
Kyle Gunter <kylewgunter@gmail.com>
2021-06-04 11:36:56 -0500

    PseudoCode

--- optimal_change.py
+++ optimal_change.py
@@ -1,9 +1,28 @@
 
 # branch Kyle-Solution
-def optimal_change (item_cost, amount_paid)
+
+# Problem set
+# > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+
+# optimal_change(31.51, 50)
+# > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
+
+
+# Pseudo Code
 
 
 
 
+def optimal_change (item_cost, amount_paid):
+    print(item_cost, amount_paid)
 
-# Write your solution here!
+
+
+
+optimal_change(62.12, 100)
+
+# optimal_change(62.13, 100)
+# > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+
+# optimal_change(31.51, 50)
+# > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,10 +1,13 @@
-import unittest
-# from roman_numerals import to_roman
+# import unittest
+# from optimal_change import optimal_change
 
 
 
 class OptimalChangeTestCase(unittest.TestCase):
-    # def test_output(self):
+    
+
+    def test_output(self):
+        print("Yes")
         # self.assertEqual(to_roman(1), 'I')
         # self.assertEqual(to_roman(3), 'III')
         # self.assertEqual(to_roman(4), 'IV')
@@ -22,6 +25,12 @@ class OptimalChangeTestCase(unittest.TestCase):
         # self.assertNotEqual(to_roman(4), 'IIII')
 
 
+
+# optimal_change(62.13, 100)
+# > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+
+# optimal_change(31.51, 50)
+# > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
 
 if __name__ == '__main__':
     unittest.main() 
