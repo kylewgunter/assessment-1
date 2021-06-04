45f8cc2154f1b218a939a8769379ff518bf35bf8
Kyle Gunter <kylewgunter@gmail.com>
2021-06-04 11:27:35 -0500

    File Setup

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,7 @@
+def optimal_change (item_cost, amount_paid)
+
+
+
+
+
 # Write your solution here!
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,11 @@
-# Write your unit tests here!
+import unittest
+# from roman_numerals import to_roman
+
+
+
+class RomanNumeralsTestCase(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main() 
