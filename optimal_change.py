698a3354708fafe406867000522c3bfdb5faaa63
Kyle Gunter <kylewgunter@gmail.com>
2021-07-09 21:31:00 -0500

    Kyle Revised

--- optimal_change.py
+++ optimal_change.py
@@ -1,107 +1,110 @@
-import decimal
 import math
-from typing import KeysView
-# branch Kyle-Solution
-
-# Problem set
-# > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
-
-# optimal_change(31.51, 50)
-# > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
-
+import re
 
 # Pseudo Code
-# import math for float int/ math.floor/ math fixed/decimal?
-# item_cost = 1st arg
-# Create a dict obj that equals key and values of '$20' : 20
-# Create empty lists for key and values
-# iterate over dict and output to iterable lists
-# 
+# 1. If- item cost and amount paid == 0 return 0 and "No transaction"
+
+# 2.
+# Elif- item cost is greater than amount paid 
+# return "Item too expensive"
+
+# 3.
+# Else - construct a dictonary of money currency values * 100 to elimnate floats.
+# remainder = 100 * amount paid - Item cost to eliminate decimals.
+# construct answer string variable
+# construct a change dictionary for the answer.
+
+# while remainder is greater than 0, iterate over currency keys, values.
+# change dictionary equals to result var
+# Remainder var uses math round of remainder minus the result and multiplied by the current value and force it to two decimal places to eliminate decimals. returns the result to remainder value in the while loop.
+
+# ***Run answer string function that takes in change dict, amount paid, item cost and output string
+# create initial output string of optimal change and amount paid
+# iterate over change dict for keys and values
+# import re for regex search
+# create change string to find the values of change types- quarter, dime, nickle, penn
+# create search for change string
+# construct output string if value is greater than 1 and no match for regex search... make dollar values output "bills"
+# construct output string if value is equal to 1 and no match for regex search... make dollar values output "bill"
+# construct output string if value is greater than 1 and match for regex search... make change values add "s"
+# construct output string if value is equal to 1 and match for regex search... return singular change values
+# Nested elif statement for "penn"... if value is equal to 1 - add 'y', if value is greater than 1, add 'ies'.
 
 
-def optimal_change (item_cost, amount_paid, ):
-    # print(item_cost, amount_paid)
 
-    dict_money_values = {
-        '$100': 100,
-        '$50': 50,
-        '$20': 20,
-        '$10': 10,
-        '$5': 5,
-        '$1': 1,
+def optimal_change (item_cost, amount_paid):
+    # 1.
+    if item_cost and amount_paid == 0:
+        print("No transaction")
+    
+    # 2.
+    elif item_cost > amount_paid:
+        print("Item too expensive!")
+    # 3.
+    else:
+        dict_currency = {
+            '$100': 10000,
+            '$50': 5000,
+            '$20': 2000,
+            '$10': 1000,
+            '$5': 500,
+            '$1': 100,
+            'quarter': 25,
+            'dime': 10,
+            'nickle': 5,
+            'penn': 1
+            }
+        # eliminate possible float issues by multiplying by 100.
+        remainder = 100 * (amount_paid - item_cost)        
+        
+        # answer dict
+        # answer string
+        answer = ""
+        change_dict = {
         }
-    
-    dict_change_values = {
-        'quarter': 25,
-        'dime': 10,
-        'nickle': 5,
-        'penny': 1
-    }
-    
-    remainder_int = math.floor(amount_paid - item_cost) # remainder value whole number
-    remainder_float = str(amount_paid - item_cost)[2:]
-    
-    change_decimal_list = remainder_float.split('.')
-    change_int = int(change_decimal_list[1])
+ 
+    # loop through remainder value until value is zero
+    while remainder > 0:
+        
+        for key in dict_currency:
+            value = dict_currency[key]      
+            result = math.floor(remainder/value)
 
-    # print(remainder_int, change_int) # 37, 88
-
-    money_currency_list = [] # target list for appended dict.keys
-    money_value_list = [] # target list for appended dict.values
-
-
-     # iterate over dict using items -- appends list vars with iterable list of keys and values
-    for money_strs, money_value in dict_money_values.items(): # 
-            money_currency_list.append(money_strs) 
-            money_value_list.append(money_value)        
-
-    # print(money_currency_list, money_value_list)
-
-    change_currency_list = []
-    change_value_list = []
-
-    for change_strs, change_value in dict_change_values.items(): # 
-            change_currency_list.append(change_strs) 
-            change_value_list.append(change_value) 
-    
-    # print(change_currency_list, change_value_list)
-
-    dollar_list = []
-
-    i = 0
-    while remainder_int > 0:
-        for x in range(math.floor(remainder_int /money_value_list[i])): 
-            dollar_list.append(money_currency_list[i])
-            remainder_int -= money_value_list[i]
-        i += 1 
-
-    change_list = list()
-    change_list_result = []
-    change_dict = {}   
-    count = 0
-    
-    j = 0
-    while change_int > 0:
-        for x in range(math.floor(change_int/change_value_list[j])):
-            if change_currency_list[j]:  
-                change_list.append([change_currency_list[j], count + 1])
-            change_int -= change_value_list[j]
+            if result > 0:
+                change_dict[key] = result 
+                remainder = round(remainder - (result * value), 2)
             
-        j += 1 
+        # print(change_dict)
+        # call anwer output function
+        answer = answer_output(item_cost, amount_paid, change_dict, answer)
+        
+        # print(answer)
+        return answer
+
+def answer_output(item_cost, amount_paid, change_dict, output):
     
-    print(change_list)
-    for elem in change_list(len(change_list)):
-        print(elem)
-    # for elem in change_list:
-    #     print(elem)
-    #     change_list.index(0)
-    # for elem in range(change_list):
-    #         print(elem)
+    output += f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is "
 
-optimal_change(62.12, 100)
+    for key in change_dict:
+        value = change_dict[key]
+          
+        change_str = 'quarter, dime, penn'
+        change_search = re.search(key, change_str)
 
-    # optimal_change(62.13, 100)
-    # > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+        if value > 1 and change_search == None:
+            output += f"{value} {key} bills, "
+        elif value == 1 and change_search == None:
+            output += f"{value} {key} bill, "
+        elif value > 1 and key != "penn":
+            output += f"{value} {key}s, "
+        elif value == 1 and key != "penn":
+            output += f"{value} {key}, "
+        elif key == "penn":
+            if value > 1:
+                output += f"and {value} {key}ies."
+            else:
+                output += f"and {value} {key}y."
+        
+    return output
 
-    # optimal_change(31.51, 50)
-    # > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
+# optimal_change(31.51, 50)
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,33 +1,25 @@
 import unittest
 from optimal_change import optimal_change
 
-
-
 class OptimalChangeTestCase(unittest.TestCase):
     
-    # Testing is output is a string
-    def test_str(self):
-        self.assertEqual(optimal_change(62.12, 100), str)
-    
-    
+    # Testing is output is a string   
+    def test_returns_a_str(self):
+        """When you call optimal_change(), you should get a str back"""
+        self.assertEqual(type(optimal_change(62.13, 100)), str)
+     
     # Testing for correct output
     def test_output(self): 
-        self.assertEqual(optimal_change(62.12, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+        self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
         self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+    # testing for transaction 
+    def test_transaction(self): 
+        self.assertNotEqual(optimal_change(0, 0), "No transaction")
     
-    # Testing for an output value
-    def test_output(self): 
-        self.assertNotEqual(optimal_change(62.12, 100), None)
-
-
-
-
-
-# optimal_change(62.13, 100)
-# > "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
-
-# optimal_change(31.51, 50)
-# > "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
+    # Item cost greater than amount paid returns an error
+    def transaction_error(self): 
+        self.assertEqual(optimal_change(100, 62.13), "Item too expensive!")
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
